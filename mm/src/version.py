790803351ff27906b20a38bb8aab88249514355e
Andy Liu <andy@madmachine.io>
2023-07-16 21:10:11 +0800

    Modify version to 0.9.0
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- mm/src/version.py
+++ mm/src/version.py
@@ -5,7 +5,7 @@
 # This is the Python 3 version of option 3 in:
 # https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
 
-__VERSION__ = '0.7.0'
+__VERSION__ = '0.9.0'
 #
 # MAINTAINERS:
 #
