184956f8624cf11c730e797da67781d309ad2b14
Andy Liu <andy@madmachine.io>
2023-08-25 22:10:13 -0700

    Modify for release/5.9
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- mm/src/spm.py
+++ mm/src/spm.py
@@ -134,9 +134,9 @@ def build(p_type, destination, dest_data):
 
     if p_type == 'executable':
         log.inf('Building executable...')
-        linker_flags = json.loads(dest_data).get('extraLinkerFlags')
-        linker_flags = ['-Xlinker ' + item for item in linker_flags]
-        flags += linker_flags
+        #linker_flags = json.loads(dest_data).get('extraLinkerFlags')
+        #linker_flags = ['-Xlinker ' + item for item in linker_flags]
+        #flags += linker_flags
     else:
         log.inf('Building library...')
 
